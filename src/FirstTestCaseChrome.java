f9950c4d500bd72130d447c13d4c3c77d4e4a0b7
Alan Liew <alanliew@Alans-Air.realtek>
2022-05-24 23:37:06 +0800

    Initial commit

---
+++ src/FirstTestCaseChrome.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstTestCaseChrome {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/alanliew/Downloads/Driver and Jar file/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:google.com");
+        System.out.println(driver.getTitle());
+        driver.close();
+    }
+}
+
+
---
+++ src/SecondTestCaseFireFox.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SecondTestCaseFireFox {
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.firefox.driver", "/Users/alanliew/Downloads/Driver and Jar file/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https:google.com");
+        System.out.println(driver.getTitle());
+        driver.close();
+
+
+    }
+}
