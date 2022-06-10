dc18610f766ef790a2258dced1256f01ee05ad12
alanliew8888 <keanoon03@gmail.com>
2022-06-11 00:06:19 +0800

    Locator - id , name , xpath , css selector and linktext

---
+++ src/Locator.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+// testing using different locators  ID , name ,linktext,  Xpath,
+public class Locator {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/alanliew/Downloads/Driver and Jar file/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:login.salesforce.com");
+        driver.manage().window().maximize();
+        //by id
+        driver.findElement(By.id("username")).click();
+        driver.findElement(By.id("username")).sendKeys("abc@gmail.com");
+        // by name
+        driver.findElement(By.name("pw")).click();
+        driver.findElement(By.name("pw")).sendKeys("12345678");
+        //by xpath
+        driver.findElement(By.linkText("Forgot Your Password?")).isDisplayed();
+        driver.findElement(By.xpath("//*[@id='Login']")).isEnabled();
+        driver.findElement(By.xpath("//*[@id=\'rememberUn\']")).isDisplayed();
+        //by csspath
+        driver.findElement(By.cssSelector("#username")).clear();
+        driver.findElement(By.cssSelector("#password")).clear();
+        driver.findElement(By.cssSelector("#password")).sendKeys("456789");
+
+
+
+
+
+        driver.quit();
+    }
+}
