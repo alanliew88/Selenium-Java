8e424fc402497f7c2501c64489af1934d1051caa
alanliew8888 <keanoon03@gmail.com>
2022-06-16 00:23:41 +0800

    Xpath (Self create xpath using tagname, attribute and value) with functions of contains and starts-with.

---
+++ src/Locator_xpath.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+
+public class Locator_xpath {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+
+
+        //using self create basic xpath //tagname[@attribute, 'value']
+        driver.findElement(By.xpath("//input[@name='search_query']"));
+
+
+        //using self create xpath //tagname[contains(@attribute, 'value')]
+        driver.findElement(By.xpath("//input[contains(@name, '_query')]")).sendKeys("Apple");
+        Thread.sleep(2000);
+
+        //using self create xpath //tagname[starts-with(@attribute, 'value')]
+        WebElement searchbox = driver.findElement(By.xpath("//input[starts-with(@name, 'search')]"));
+        searchbox.clear();
+        searchbox.sendKeys("Orange");
+        Thread.sleep(2000);
+
+
+
+        driver.close();
+
+
+    }
+}
