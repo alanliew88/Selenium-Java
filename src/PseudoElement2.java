8330e2b31de8e2f63621e5a36790a5bf6ccc452e
alanliew8888 <keanoon03@gmail.com>
2022-06-16 00:00:56 +0800

    Pseudo element - Another method to counter pseudo element

---
+++ src/PseudoElement2.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class PseudoElement2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/alanliew/Downloads/Driver and Jar file/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://agoda.com");
+
+        Thread.sleep(5000);
+
+        //Actions action = new Actions(driver);
+        //WebElement closebutton = driver.findElement(By.tagName("svg"));
+        //action.moveToElement(closebutton).click().build().perform();
+
+
+        //WebElement closebutton = driver.findElement(By.className("ab-close-button"));
+        //closebutton.isDisplayed();
+        //closebutton.click();
+
+
+        driver.quit();
+        System.out.println("Success");
+    }
+}
