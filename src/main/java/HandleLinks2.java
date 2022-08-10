6c94309b61828dd4e681cdef8267f7ea0955c00a
alanliew8888 <keanoon03@gmail.com>
2022-08-10 23:26:26 +0800

    Extracting all the <a> tag text and href attribute

---
+++ src/main/java/HandleLinks2.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class HandleLinks2 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://deadlinkcity.com");
+
+        driver.manage().window().maximize();
+
+        driver.findElement(By.partialLinkText("Errorcode 400"));
+
+        List<WebElement> semualinks = driver.findElements(By.tagName("a"));
+
+        for (WebElement single:semualinks){
+            System.out.println(single.getText());
+            System.out.println(single.getAttribute("href"));
+        }
+
+        driver.quit();
+
+
+
+    }
+}
